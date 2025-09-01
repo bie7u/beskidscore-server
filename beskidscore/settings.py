2bbdfb8e45162ce70f57569011b87ccd4eaeb2b7
Biel <krystiantsp@gmail.com>
2025-09-01 12:21:54 +0200

    Cors headers

--- beskidscore/settings.py
+++ beskidscore/settings.py
@@ -70,7 +70,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:5174",
-    "http://127.0.0.1:5174"
+    "http://127.0.0.1:5174",
+    "http://100.64.0.1:5173"
+
 ]
 
 
